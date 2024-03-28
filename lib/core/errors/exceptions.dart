f90c5170d83444db0bad0f3c936a16dfcb0f684d
Kilian <kilian.dcs@gmail.com>
2024-03-28 14:04:36 +0100

    adding exceptions

--- lib/core/errors/exceptions.dart
+++ lib/core/errors/exceptions.dart
@@ -1,4 +1,6 @@
+import 'package:gymprime/core/enums/model_type.dart';
 import 'package:http/http.dart' as http;
+import 'package:objectid/objectid.dart';
 
 class ServerException implements Exception {
   final http.Response response;
@@ -9,3 +11,23 @@ class ServerException implements Exception {
 }
 
 class CacheException implements Exception {}
+
+class NoRowAffectedException extends CacheException {}
+
+class MultipleRowsAffectedException extends CacheException {}
+
+class NoRowInsertedException extends CacheException {}
+
+class CacheInconsistencyException extends CacheException {
+  CacheInconsistencyException({
+    required List<Object?> cache,
+    required Object? cachedObject,
+  });
+}
+
+class UnknownIdException extends CacheException {
+  UnknownIdException({
+    required ModelType modelType,
+    required ObjectId id,
+  });
+}
